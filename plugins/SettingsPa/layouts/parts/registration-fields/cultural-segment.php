d1dafe7748f30e41119a28b35f00b51009a42192
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-10-20 10:56:37 -0300

    corrige template part ultural-segment.php

--- plugins/SettingsPa/layouts/parts/registration-fields/cultural-segment.php
+++ plugins/SettingsPa/layouts/parts/registration-fields/cultural-segment.php
@@ -11,4 +11,4 @@ $this->import(
     'entity-terms'
 );
 ?>
-<entity-terms :entity="entity" taxonomy="segmento" :editable="isEditable ? 'true' : 'false' ?>" classes="col-12" title="<?php i::_e('Segmento cultural'); ?>" ></entity-terms>
+<entity-terms :entity="entity" taxonomy="segmento" :editable="<?= $isEditable ? 'true' : 'false' ?>" classes="col-12" title="<?php i::_e('Segmento cultural'); ?>" ></entity-terms>
